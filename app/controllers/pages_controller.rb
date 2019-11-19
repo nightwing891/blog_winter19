aa6193e4a05916fd82549cdfb5c3dca01f3a2cc5
Henry Doan <henrydoan96@gmail.com>
2019-11-18 21:00:49 -0700

    end

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,14 @@
 class PagesController < ApplicationController
+  # Notescontroller
   def index
     @pages = Page.all
+    # @notes = Note.all
     # render index.html.erb
   end
 
   def show
     @page = Page.find(params[:id])
+    # @note = note.find(params[:id])
     # render show.html.erb
   end
 
@@ -29,6 +32,7 @@ class PagesController < ApplicationController
 
   private
     def pages_params
+      # { page: { title: 'adsf', author: '', body: '' }}
       params.require(:page).permit(:title, :author, :body)
     end
 end
